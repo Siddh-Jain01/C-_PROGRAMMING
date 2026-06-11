236c2500902d143b99d1561ef0e17026841374b5
siddh-jain01 <siddhjain430@gmail.com>
2026-06-11 11:03:33 +0530

    new code

--- 01.BASICS/05_Swapping.cpp
+++ 01.BASICS/05_Swapping.cpp
@@ -1,15 +1,15 @@
-// sturctured c++ 
-#include<iostream>
-using namespace std ;
-int main(){
-    int a,b;
-    cout<<"enter the value a & b :";
-    cin>>a>>b;
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    cout<<"after swapping"<<a<<b;
-    
-    
+// Swap Two Numbers Without Using a Temporary Variable
+#include <iostream>
+using namespace std;
 
-}
+int main() {
+    int a, b;
+    cout << "Enter two numbers a and b: ";
+    cin >> a >> b;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0; }
